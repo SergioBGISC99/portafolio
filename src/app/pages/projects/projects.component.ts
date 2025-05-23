812a8823892308038f075098caad2a4d1e7be28b
Sergio Barreras <sergiobg.isc@gmail.com>
2025-05-23 16:09:09 -0700

    proyecto de cacei agregado

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -23,5 +23,17 @@ export class ProjectsComponent {
         { name: 'Cloudinary', logo: '/icons/cloudinary.svg' },
       ],
     },
+    {
+      title: 'CACEI ITESCA',
+      description:
+        'Plataforma web desarrollada para el Instituto Tecnologico Superior de Cajeme para la generación de cédulas requeridas por el organismos acreditador CACEI.',
+      adminUrl: 'https://cacei.servicios.itesca.edu.mx/auth/login',
+      tech: [
+        { name: 'Angular', logo: '/icons/angular.svg' },
+        { name: 'Tailwind', logo: '/icons/tailwindcss.svg' },
+        { name: 'Django', logo: '/icons/django.svg' },
+        { name: 'SQLite', logo: '/icons/sqlite.svg' },
+      ],
+    },
   ];
 }
