446532242884a33334b84bb8bf138656695d33f0
Sergio Barreras <sergiobg.isc@gmail.com>
2025-06-05 05:42:02 -0700

    formulario de contacto

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+  fb = inject(FormBuilder);
+  enviado = false;
 
+  form = this.fb.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    message: ['', [Validators.required]],
+  });
+
+  async onSubmit() {
+    if (this.form.invalid) return;
+
+    const formData = new FormData();
+    formData.append('name', this.form.value.name ?? '');
+    formData.append('email', this.form.value.email ?? '');
+    formData.append('message', this.form.value.message ?? '');
+    formData.append('_captcha', 'false');
+
+    try {
+      await fetch('https://formsubmit.co/ajax/sergiobg.isc@gmail.com', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+        },
+        body: formData,
+      });
+
+      this.enviado = true;
+
+      this.form.reset();
+    } catch (err) {
+      alert('Hubo un error al enviar el mensaje');
+    }
+  }
 }
