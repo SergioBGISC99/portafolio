3e3fc995a6b1bf848616924fedf2bf280a314498
Sergio Barreras <sergiobg.isc@gmail.com>
2025-06-28 12:25:53 -0700

    Restructures application for a single-page layout
    Refactors the application to utilize a single-page layout, integrating all components directly into the main application component.
    
    This change removes the routing module and associated page components, streamlining navigation and improving initial load time.
    The CV PDF has also been updated.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from './shared/navbar/navbar.component';
+
+import { HomeComponent } from './pages/home/home.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
+import { TecnologiesComponent } from './pages/tecnologies/tecnologies.component';
+import { CoursesComponent } from './pages/courses/courses.component';
+import { ContactComponent } from "./pages/contact/contact.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent],
+  imports: [
+    HomeComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    TecnologiesComponent,
+    CoursesComponent,
+    ContactComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
